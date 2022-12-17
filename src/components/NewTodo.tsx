8cf65bcdaf6be9fbacdc9e245f2f775037c6f788
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2022-12-17 16:14:07 +0200

    add new todo form in ts

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NewTodo = () => {
+
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <label htmlFor='text'>Todo text</label>
+            <input id='text' type='text'/>
+            <button>Add Todo</button>
+        </form>
+    );
+};
+
+export default NewTodo;
