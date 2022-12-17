315f4dad28ddaefff5741972db82ee6209b9fb4b
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2022-12-17 15:47:23 +0200

    add todo data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Todos from "./components/Todos";
+import Todo from './models/todo';
 import './App.css';
 
 function App() {
+    const todos = [
+        new Todo("Learn React"),
+        new Todo("Learn TypeScript")
+    ]
+
     return (
-        <Todos items={
-            ['Learn React', 'Learn Typescript']
-        }/>
+        <Todos items={todos}/>
     );
 }
 
